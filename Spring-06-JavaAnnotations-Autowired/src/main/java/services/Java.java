252c7eee8814af76f3d7b95c2874f0591515a83f
kayamustafa1958 <kayamustafa1958@gmail.com>
2020-10-25 22:20:31 -0500

    Autowiring with different ways

--- Spring-06-JavaAnnotations-Autowired/src/main/java/services/Java.java
+++ Spring-06-JavaAnnotations-Autowired/src/main/java/services/Java.java
@@ -6,15 +6,29 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
+    @Autowired
     private OfficeHours officeHours;
 
-    @Autowired  // if we do have more than one constructor we need to at least one autowired, if we have one no need to put @Autowired
-    public Java(OfficeHours officeHours) {
+    // @Autowired  // if we do have more than one constructor we need to at least one autowired, if we have one no need to put @Autowired
+    ////this is constructor injection
+    // public Java(OfficeHours officeHours) {
+    //   this.officeHours = officeHours;
+    //}
+
+
+   /* @Autowired
+    public void setOfficeHours(OfficeHours officeHours) {
         this.officeHours = officeHours;
     }
+*/
+
+    public OfficeHours getOfficeHours(){
+        return officeHours;
+    }
+
 
     @Override
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours : "+(30+officeHours.getHours()));
+        System.out.println("Weekly teaching hours : " + (30 + officeHours.getHours()));
     }
 }
