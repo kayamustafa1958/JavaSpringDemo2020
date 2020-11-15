1c42e0af2b8b6ecc769f37cb261c64c3fb2e6233
kayamustafa1958 <kayamustafa1958@gmail.com>
2020-11-15 09:01:03 -0600

    Path parameters

--- Spring-11-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
+++ Spring-11-SpringBoot-MVC-Controller/src/main/java/com/cybertek/controller/HomeController.java
@@ -1,10 +1,7 @@
 package com.cybertek.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 
@@ -37,6 +34,19 @@ public class HomeController {
 
     @PostMapping("/spring")
     public String postMappingEx(){
+        return "home";
+    }
+
+    @GetMapping("/home/{name}")
+    public String pathVariableEx(@PathVariable("name") String name){
+
+        System.out.println("name is: "+name);
+        return "home";
+    }
+    @GetMapping("/home/{name}/{email}")
+    public String pathVariableEx2(@PathVariable("name") String name,@PathVariable("email") String email) {
+
+        System.out.println("name is: " + name+" and email is "+email);
         return "home";
     }
 
