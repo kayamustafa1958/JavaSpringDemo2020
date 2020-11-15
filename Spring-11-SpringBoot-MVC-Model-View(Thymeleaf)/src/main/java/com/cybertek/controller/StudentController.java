e9b0a8d27cbf8172a999c2ace8153d8d9f163ce5
kayamustafa1958 <kayamustafa1958@gmail.com>
2020-11-15 14:15:42 -0600

    Binding with HTML

--- Spring-11-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/cybertek/controller/StudentController.java
+++ Spring-11-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/cybertek/controller/StudentController.java
@@ -1,9 +1,15 @@
 package com.cybertek.controller;
 
+import com.cybertek.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 @RequestMapping("/student")
@@ -12,13 +18,38 @@ public class StudentController {
     @GetMapping("/welcome")
     public String homePage(Model model) {
 
-        model.addAttribute("name","Kaya");  // attribute name and html attribute name should match
-        model.addAttribute("course","MVC");  // attribute name and html attribute name should match
+        model.addAttribute("name", "Kaya");  // attribute name and html attribute name should match
+        model.addAttribute("course", "MVC");  // attribute name and html attribute name should match
 
-        String subject="Collections";
-        model.addAttribute("subject",subject);
+        String subject = "Collections";
+        model.addAttribute("subject", subject);
+
+        int studentId = new Random().nextInt(1000);
+        model.addAttribute("id", studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(7);
+        model.addAttribute("numbers", numbers);
+
+        LocalDate birthday = LocalDate.now().minusYears(42);
+        model.addAttribute("birthday", birthday);
+
+        Student student = new Student(1, "Mike", "Smith");
+        model.addAttribute("student", student);
 
         return "student/welcome";
     }
 
+    @GetMapping("/register")
+    public String homePage2() {
+
+
+
+
+
+
+        return "student/register";
+    }
 }
