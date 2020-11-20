68800288ff002993fc722c6423d9009c43f1b476
kayamustafa1958 <kayamustafa1958@gmail.com>
2020-11-19 23:10:40 -0600

    Form is binded with the model

--- Spring-11-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
+++ Spring-11-SpringBoot-MVC-Form/src/main/java/com/cybertek/controller/EmployeeController.java
@@ -1,20 +1,26 @@
 package com.cybertek.controller;
 
+import com.cybertek.dataGenerator.DataGenerator;
+import com.cybertek.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/employee")
 public class EmployeeController {
 
     @GetMapping("/register")
-public String employeeCreate(){
+    public String employeeCreate(Model model) {
 
+        model.addAttribute("employee", new Employee());
 
+        model.addAttribute("stateList",DataGenerator.getStateList());
         return "/employee/employee-create";
     }
-
 
 
 }
