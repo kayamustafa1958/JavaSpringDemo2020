656ec02243161f539de99d77e722bf593fc64ff3
kayamustafa1958 <kayamustafa1958@gmail.com>
2021-01-01 10:10:51 -0600

    table and column name define

--- Spring-13-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
+++ Spring-13-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
@@ -1,11 +1,13 @@
 package com.cybertek.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+
 @Entity
 public class Student {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+    @Column(name="studenFirstName")
     private String firstName;
     private String lastName;
     private String email;
--- Spring-13-ORM-TableCreation/src/main/resources/data.sql
+++ Spring-13-ORM-TableCreation/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT  into student(id,first_name,last_name,email) VALUES (1,'Mike','Smith','mike@gmail.com')
-INSERT  into student(id,first_name,last_name,email) VALUES (2,'Adam','Shaun','adam@gmail.com')
-INSERT  into student(id,first_name,last_name,email) VALUES (3,'Tom','Hanks','tom@gmail.com')
-INSERT  into student(id,first_name,last_name,email) VALUES (4,'Brad','Kelly','brad@gmail.com')
+INSERT  into student(first_name,last_name,email) VALUES ('Mike','Smith','mike@gmail.com')
+INSERT  into student(first_name,last_name,email) VALUES ('Adam','Shaun','adam@gmail.com')
+INSERT  into student(first_name,last_name,email) VALUES ('Tom','Hanks','tom@gmail.com')
+INSERT  into student(first_name,last_name,email) VALUES ('Brad','Kelly','brad@gmail.com')
 
