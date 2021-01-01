114318661eae44482b2b6d4c257a78aee9adcbee
kayamustafa1958 <kayamustafa1958@gmail.com>
2021-01-01 01:11:02 -0600

    ORM cinnections

---
+++ Spring-13-ORM-TableCreation/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT  into student(id,first_name,last_name,email) VALUES (1,'Mike','Smith','mike@gmail.com')
+INSERT  into student(id,first_name,last_name,email) VALUES (2,'Adam','Shaun','adam@gmail.com')
+INSERT  into student(id,first_name,last_name,email) VALUES (3,'Tom','Hanks','tom@gmail.com')
+INSERT  into student(id,first_name,last_name,email) VALUES (4,'Brad','Kelly','brad@gmail.com')
+
